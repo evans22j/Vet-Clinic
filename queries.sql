a0994c3483bd2eb9d83fdef84c9847284cb23809
Evans Sitibekiso <jevacrism@gmail.com>
2022-06-10 14:25:57 +0200

    delete animals born after jan

--- queries.sql
+++ queries.sql
@@ -37,3 +37,13 @@ BEGIN TRANSACTION;
 DELETE FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+-- Inside a transaction Delete all animals born after JAN-01-2022;
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE  date_of_birth > '2022-01-01';
+SAVEPOINT SAVEPOINT_1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT_1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
+COMMIT;
+SELECT weight_kg FROM animals;
