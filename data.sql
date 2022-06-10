72dde723ee0288429987ea85e10a23f8d4eb72c9
Evans Sitibekiso <jevacrism@gmail.com>
2022-06-10 20:35:27 +0200

    include owner information

--- data.sql
+++ data.sql
@@ -42,3 +42,15 @@ WHERE name = 'Agumon';
 UPDATE animals
 SET owner_id = (SELECT id from owners WHERE full_name = 'Jennifer Orwell')
 WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = (SELECT id from owners WHERE full_name = 'Bob')
+WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals
+SET owner_id = (SELECT id from owners WHERE full_name = 'Melody Pond')
+WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals
+SET owner_id = (SELECT id from owners WHERE full_name = 'Dean Winchester')
+WHERE name = 'Angemon'  OR name = 'Boarmon';
