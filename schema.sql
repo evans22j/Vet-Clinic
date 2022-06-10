45cf53963bf5eaa22eb9ab1beb8fd6fae4ae17af
Evans Sitibekiso <jevacrism@gmail.com>
2022-06-10 14:37:01 +0200

    drop table of animals

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+DROP TABLE animals;
+
 CREATE TABLE animals (
     id BIGSERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	date_of_birth DATE NOT NULL,
 	escape_attempt INT,
 	neutered BOOLEAN NOT NULL,
-	weight_kg DECIMAL NOT NULL
+	weight_kg DECIMAL(10, 2) NOT NULL,
+	species varchar(100)
 );
